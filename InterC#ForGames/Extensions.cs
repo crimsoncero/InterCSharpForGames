c8bcc3adcb17698b612f95d0d3212764f8755d5e
Amit Breiman <crimson.breiman@gmail.com>
2024-04-05 17:10:09 +0300

    Linq hw

---
+++ InterC#ForGames/Extensions.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Collections;
+
+namespace InterC_ForGames
+{
+    public static class Extensions
+    {
+
+        public static void PrintCollection(this IEnumerable coll)
+        {
+            foreach (var item in coll)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static T GetHighest<T,TKey>(this IEnumerable<T> coll, Func<T,TKey> keySelector)
+        {
+            var orderedColl = coll.OrderByDescending(keySelector);
+            return orderedColl.First();
+        }
+
+
+    }
+
+
+}
--- InterC#ForGames/Program.cs
+++ InterC#ForGames/Program.cs
@@ -1,72 +1,76 @@
 ﻿
+using System.Net.Security;
+
 namespace InterC_ForGames
 {
     class Program
     {
         public static void Main(String[] args)
         {
-            
-
-            int ElvesWinsAgainstHumans = 0;
-            int ElvesWinsAgainstMonsters = 0;
-            int HumanWinsAgainstElves = 0;
-            int HumanWinsAgainstMonsters = 0;
-            int MonstersWinsAgainstElves = 0;
-            int MonstersWinsAgainstHumans = 0;
-
-            int numOfIterations = 10_000;
-            int numOfUnits = 4;
-
-
-            // Humans V Elves
-
-            for (int i = 0; i < numOfIterations; i++)
+            Console.WriteLine("Distinct Ascending: ");
+            foreach (var t in DistinctAscending(new int[] { 1, 45, 2, 1, 2, 2, 41, -1 }))
             {
-                Actor actor1 = new Actor("Bob", Race.Human, numOfUnits, (10, 15));
-                Actor actor2 = new Actor("Mark", Race.Elf, numOfUnits, (10, 15));
-                switch(Combat.DuelFast(actor1, actor2, isAuto: true))
-                {
-                    case 1: HumanWinsAgainstElves++; break;
-                    case -1: ElvesWinsAgainstHumans++; break;
-                }
-            }
-            
-            // Humans V Monstres
-
-            for (int i = 0; i < numOfIterations; i++)
-            {
-                Actor actor1 = new Actor("Bob", Race.Human, numOfUnits, (10, 15));
-                Actor actor2 = new Actor("Mark", Race.Monster, numOfUnits, (10, 15));
-                switch (Combat.DuelFast(actor1, actor2, isAuto: true))
-                {
-                    case 1: HumanWinsAgainstMonsters++; break;
-                    case -1: MonstersWinsAgainstHumans++; break;
-                }
+                Console.WriteLine(t);
             }
 
-            // Elves V Monstres
+            Console.WriteLine("\nAverage Word Length:");
+            Console.WriteLine(AverageWordLength("Bla bla wooohoo, wdalwalf"));
 
-            for (int i = 0; i < numOfIterations; i++)
+            Console.WriteLine("\nStarts And Ends With:");
+            foreach (var t in StartsAndEndsWith(new string[] { "Oy Hellos", "There", "General", "Helloooofo", "OnlyHans" }, 'O', 's'))
             {
-                Actor actor1 = new Actor("Bob", Race.Elf, numOfUnits, (10, 15));
-                Actor actor2 = new Actor("Mark", Race.Monster, numOfUnits, (10, 15));
-                switch (Combat.DuelFast(actor1, actor2, isAuto: true))
-                {
-                    case 1: ElvesWinsAgainstMonsters++; break;
-                    case -1: MonstersWinsAgainstElves++; break;
-                }
+                Console.WriteLine(t);
             }
 
-            Console.Clear();
-            Console.WriteLine("Battle Simulation");
-            Console.WriteLine($"Number of Iterations : {numOfIterations}");
-            Console.WriteLine($"Number of Units : {numOfUnits}\n");
-            Console.WriteLine($"Humans VS Elves : {HumanWinsAgainstElves} : {ElvesWinsAgainstHumans}\n");
-            Console.WriteLine($"Humans VS Monsters : {HumanWinsAgainstMonsters} : {MonstersWinsAgainstHumans}\n");
-            Console.WriteLine($"Monsters VS Elves : {MonstersWinsAgainstElves} : {ElvesWinsAgainstMonsters}\n");
+
+            List<Enemy> es = new();
+            for (int i = 0; i < 150; i++)
+            {
+                es.Add(new Enemy() { Damage = Random.Shared.Next(1, 150) });
+            }
+            // would print the enemy with the higest damage
+            Console.WriteLine(es.GetHighest(e => e.Damage));
+            Console.WriteLine();
+
         }
+
+        public static IEnumerable<T> DistinctAscending<T>(IEnumerable<T> collection)
+        {
+            return collection.Distinct().OrderBy(x => x);
+        }
+
+        public static int AverageWordLength(string str)
+        {
+            string[] words = str.Split(' ', ',', '.', ':', ';', '?', '!');
+            int sum = 0;
+
+            foreach (string word in words)
+            {
+                sum += word.Length;
+            }
+
+            return sum / words.Count();
+        }
+
+        public static IEnumerable<string> StartsAndEndsWith(IEnumerable<string> strings, char start, char end)
+        {
+            return strings.Where((s) => s.StartsWith(start) && s.EndsWith(end));
+        }
+
+
+
     }
-}   
+
+    public class Enemy
+    {
+        public int Damage = 10;
+        public override string ToString()
+        {
+            return Damage.ToString();
+        }
+
+    }
+}
 
 
 
